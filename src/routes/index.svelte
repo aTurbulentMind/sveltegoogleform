fdf6df460949360efcaa62e63e12107125d615d9
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-02-22 11:19:46 -0800

    Add files via upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,27 @@
 <script>
-    const submitForm = async (data) => {
-        const formData = new FormData(data.currentTarget);
-        
-        const res = await fetch("contact.json", {
-            method: "POST",
-            body: formData,
-        });
-    };
+  let submitStatus;
+  const submitForm = async (data) => {
+    submitStatus = "submitting";
+    const formData = new FormData(data.currentTarget);
+
+    const res = await fetch("contact.json", {
+      method: "POST",
+      body: formData,
+    });
+
+    const { message } = await res.json();
+    submitStatus = message;
+  };
 </script>
 
-
-<body>
-    <h1>Hello Friend</h1>
-
-      <form on:submit|preventDefault={submitForm}>
+{#if submitStatus == "submitting"}
+  <p>Submitting...</p>
+{:else if submitStatus == "failed"}
+  <p>Form submission failed</p>
+{:else if submitStatus == "success"}
+  <p>Form submission successful</p>
+{:else}
+  <form on:submit|preventDefault={submitForm}>
     <div>
       <label for="">
         Name
@@ -30,16 +38,10 @@
       <input type="submit" />
     </div>
   </form>
-    
-</body>
+{/if}
 
-
-<style lang="scss">
-
-    @import 'components';
-
-    div{
-        margin-bottom: 0.5em;
-    }
-   
+<style>
+  div {
+    margin-bottom: 0.5em;
+  }
 </style>
